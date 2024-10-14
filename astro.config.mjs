ce9ca9b9b497152b642504a793729cc96c38178f
javad <javad_design@yahoo.com>
2024-10-14 06:06:57 +0330

    Change: modify github deploy action Change: site in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { remarkReadingTime } from './src/utils/readTime.ts'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blog-template-gray.vercel.app/', // Write here your website url
+	site: 'https://fum-hydroinformatics.github.io/', // Write here your website url
+	base: 'website-template',
 	markdown: {
 		remarkPlugins: [remarkReadingTime],
 		drafts: true,
