71a2694a41691fc4567fdc27bec04da1e1fcfc60
javad <javad_design@yahoo.com>
2024-10-15 05:35:57 +0330

    Change: disable 'Sharp' service for image in dev mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig, passthroughImageService  } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import { remarkReadingTime } from './src/utils/readTime.ts'
+
+const SCRIPT = process.env.npm_lifecycle_script || "";
+const isBuild = SCRIPT.includes("astro build");
+
+const SHARP_SERVICE = passthroughImageService();
+if (isBuild) {
+	SHARP_SERVICE = '';
+}
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,6 +24,9 @@ export default defineConfig({
 			wrap: true
 		}
 	},
+	image: {
+		service: SHARP_SERVICE,
+	},
 	integrations: [
 		mdx({
 			syntaxHighlight: 'shiki',
@@ -23,7 +34,7 @@ export default defineConfig({
 				experimentalThemes: {
 					light: 'vitesse-light',
 					dark: 'material-theme-palenight',
-				  },
+				},
 				wrap: true
 			},
 			drafts: true
